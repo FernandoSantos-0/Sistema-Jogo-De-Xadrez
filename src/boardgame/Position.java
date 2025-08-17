f18c1e86a765506ed063e3c23f628dc19a6611a1
Fernando-Pereira-Dos-Santos <fp354575@gmail.com>
2025-08-17 17:44:24 -0300

    Pastas criadas junto com primeira classe.

---
+++ src/boardgame/Position.java
@@ -0,0 +1,40 @@
+package boardgame;
+
+public class Position {
+
+    private int coluna;
+    private int linha;
+
+    public Position(){}
+
+    public Position(int coluna, int linha) {
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public void setValue(int coluna, int linha){
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+
+    @Override
+    public String toString() {
+        return linha + ", " + coluna;
+    }
+}
